17105ce0e88521aacdb4a7b05ee52c55a68a834d
Wayne Wang <sw@middlebury.edu>
2022-04-16 00:46:31 -0400

    modified app.js for starting template, added authors in package.json, created README

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
+
 import 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 
-import { View, Text, StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 
-
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer >
-     <View>
-       <Text>Projectttttt</Text>
-     </View>
-   
-      <StatusBar style="auto" />
-
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>CodeDash</Text>
+        <StatusBar style="light" />
+      </View>
+    </SafeAreaView>
   );
 }
 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#fff',
+  }
+});
---
+++ README.md
@@ -0,0 +1,5 @@
+# CodeDash
+
+CodeDash is a social media platform... using only code!
+
+Work in progress... coming in May 2022.
