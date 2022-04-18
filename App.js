6b4a28dc8517f303cad0ffcfb319dba2c97376d1
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-04-18 14:32:31 -0400

    added logo

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
-
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-
 import { StatusBar } from 'expo-status-bar';
-
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
+
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={styles.title}>CodeDash</Text>
+      <View style={styles.main}>
+        <Image
+          source={require("./assets/logo-codedash.png")}
+          style = {{
+            width: '100%',
+            height: '20%',
+          }}
+        ></Image>
         <StatusBar style="light" />
       </View>
     </SafeAreaView>
@@ -20,10 +24,16 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
+    height: '100%',
     flex: 1,
     backgroundColor: '#111',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  main: {
+    width: '100%',
+    height: '100%',
   },
   title: {
     fontSize: 30,
