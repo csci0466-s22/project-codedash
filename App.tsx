4061b8ac657722f2c8cde247cf56b3e9b21b41b0
Wayne Wang <sw@middlebury.edu>
2022-04-26 23:07:59 -0400

    removed unused styles in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
 import BottomTabNav from './navigation/BottomTabNav';
 
 export default function App() {
@@ -16,27 +15,3 @@ export default function App() {
 
   );
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    height: "100%",
-    flex: 1,
-    backgroundColor: "#211D33",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  main: {
-    width: "95%",
-    height: "100%",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: "bold",
-    color: "#fff",
-  },
-});
