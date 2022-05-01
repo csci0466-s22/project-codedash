11b85d7ab0ae2765b20ac6e5c7346b38fe71e873
Cliff Yang <cliffyyc@outlook.com>
2022-04-30 23:49:39 -0400

    poc done. realtime styling of written code

--- screens/PostingScreen.tsx
+++ screens/PostingScreen.tsx
@@ -1,17 +1,36 @@
-import { View, Text, StyleSheet, Button, Pressable } from "react-native";
+import { View, Text, TextInput, StyleSheet, Keyboard, TouchableWithoutFeedback} from "react-native";
 import { createStackNavigator } from '@react-navigation/stack';
 import NativeIconicIcon from '../components/NativeIconicIcon';
-
-
-
-
-const Stack = createStackNavigator();
+import {useState, useEffect} from 'react';
+import Code from "../components/Code";
+import { Language } from "prism-react-renderer";
 
 function PostingScreen({navigation}: {navigation: any}) {
+  const [textContent, changeContent] = useState('');
+  console.log(textContent);
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>New Posting Screen</Text>
-    </View >
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()} accessible={false}>
+      <View style={styles.container}>
+        <Text style={styles.text}>New Posting Screen</Text>
+          
+        <View style={styles.inputContainer}>
+          <View style={styles.overlay}>
+            <Code
+              code={textContent}
+              language={"python" as Language}
+            />
+          </View>
+          <TextInput
+          editable
+          maxLength={4000}
+          multiline={true}
+          style={styles.input}
+          value={textContent}
+          onChangeText={text => changeContent(text)}
+          />
+        </View>
+      </View >
+    </TouchableWithoutFeedback>
   )
 }
 
@@ -32,6 +51,25 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 30,
     color: "#fff",
+  },
+  inputContainer: {
+    width: "100%",
+    height: "75%",
+    borderBottomColor: '#000000',
+    borderBottomWidth: 1,
+  },
+  input: {
+    paddingTop: 20,
+    paddingLeft: 20,
+    width: "100%",
+    height: "100%",
+    color: 'transparent'
+  }, 
+  overlay: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#252526",
+    position: "absolute"
   }
 });
 
