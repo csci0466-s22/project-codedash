578bd73d6e049006b6e4d42bc2c07de2112298bf
Wayne Wang <sw@middlebury.edu>
2022-05-07 16:04:52 -0400

    changed emoji like to 'likes'

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -75,7 +75,7 @@ function Card({ post, size = "large", children}: CardProps) {
               <Text style={styles.userName}>@{post.user.name}</Text>
             </View>}
           <View style={styles.CardHeaderRight}>
-            <Text style={styles.LikeCount}>{post.voteCount + " ❤️"}</Text>
+            <Text style={styles.LikeCount}>{post.voteCount + " likes"}</Text>
             {size === "large" ?
               <TouchableOpacity onPress={onMenuPress}>
                 <NativeIconicIcon name="ellipsis-vertical" size={18} color="#fff" />
